e655ca092afd99c4de544c6458a1bb45c55f22a3
Rwaltzwills <Rwaltzwills@gmail.com>
2024-08-16 18:09:30 -0400

    Added mob, testing scene

--- player.gd
+++ player.gd
@@ -10,17 +10,17 @@ signal take_hit
 @export var y_scale_when_eating = 1.5
 @export var x_scale_when_eating = 1.5
 var velocity
-var screen_size
+var screen_size = get_viewport_rect().size
 
 func _ready() -> void:
-	velocity = 0
+	velocity = Vector2(0,0)
 
 func _physics_process(delta: float) -> void:
 	# Handle swimming
 	# Get the input direction
-	velocity = 0 # reset velocity # DEBUG: Should we have velocity slowly go towards 0 to simulate swimming?
-	var direction_lr := Input.get_axis("Left Swim", "Right Swim") # Left/Right # DEBUG: Change these to actions vice the animation names
-	var direction_ud := Input.get_axis("Down Swim", "Up Swim") # Up/down # DEBUG: Change these to actions vice the animation names
+	velocity = Vector2(0,0) # reset velocity # DEBUG: Should we have velocity slowly go towards 0 to simulate swimming?
+	var direction_lr := Input.get_axis("Left swim", "Right swim") # Left/Right # DEBUG: Change these to actions vice the animation names
+	var direction_ud := Input.get_axis("Up swim", "Down swim") # Up/down # DEBUG: Change these to actions vice the animation names
 	
 	# Get velocity
 	if direction_lr:
@@ -28,26 +28,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	if direction_ud:
-		velocity.y = direction_lr * SPEED
+		velocity.y = direction_ud * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 	# Choose animation based on direction
 	# Debug: Add all 8 directions, different comparisons for velocity eights for animation
 	if velocity.x < 0:
-		_animation_player.animation = "Left swim"
+		_animation_player.play("Left swim")
 	if velocity.x > 0:
-		_animation_player.animation = "Right swim"
+		_animation_player.play("Right swim")
 	if velocity.y < 0:
-		_animation_player.animation =  "Down swim"
+		_animation_player.play("Down swim")
 	if velocity.y > 0: 
-		_animation_player.animation = "Up swim"
+		_animation_player.play("Up swim")
 	
 	move(velocity, delta)
 
 func move(move_velocity: Vector2, delta: float):
 	position += move_velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size) # DEBUG: Define screen size
+	# position = position.clamp(Vector2.ZERO, screen_size) # DEBUG: Define screen size
 
 func _process(delta: float) -> void:
 	pass
@@ -60,6 +60,7 @@ func changeSize() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	$CollisionShape2D.set_deferred("disabled", true)
+	print(body)
 	if "eating_size" in body:
 		if self.eating_size > body.eating_size:
 			changeSize()
