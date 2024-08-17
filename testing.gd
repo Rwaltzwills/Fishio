0287893d3bff96b5370e91e79af77dad1ea140af
Rwaltzwills <Rwaltzwills@gmail.com>
2024-08-16 22:53:48 -0400

    Testing scene, building player script, mob spawning logic

--- testing.gd
+++ testing.gd
@@ -1,6 +1,4 @@
-extends CharacterBody2D
-@export var eating_size = 0
-@export var SPEED = 150
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,8 +10,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func onDeath() -> void:
-	pass
-	# Reset AI
-	# Pause for a short time
-	# Restart AI
+
+func _on_player_collided(body) -> void:
+	$"Mob Spwaner".repositionMob(body) # Replace with function body.
