b7c39db76b4096c8fad53fb6853e45c16ab9323f
Rwaltzwills <Rwaltzwills@gmail.com>
2024-08-18 22:06:12 -0400

    Fixed double scaling of size bug, temporarily disabled camera terping, toned down biggest fish

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -12,11 +12,12 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	
 	#move camera to target/s
-	if is_instance_valid(target_1):
-		var t1 = target_1.global_position
-		var t2 = get_global_mouse_position()
-		
-		if is_instance_valid(target_2):
-			t2 = target_2.global_position
-		
-		global_position = lerp(global_position,(t1+t2)/2,0.1)
+	#if is_instance_valid(target_1):
+	#	var t1 = target_1.global_position
+	#	var t2 = get_global_mouse_position()
+	#	
+	#	if is_instance_valid(target_2):
+	#		t2 = target_2.global_position
+	#	
+	#	global_position = lerp(global_position,(t1+t2)/2,0.1)
+	pass
